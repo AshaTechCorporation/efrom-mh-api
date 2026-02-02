eeabf9ad3f7e0007938e5a2beee437501146360e
Han Min Thaw <hanminthaw15@gmail.com>
2026-02-02 13:59:04 +0700

    [subtask:mh-eform-0197-subtask-01-815]

---
+++ database/migrations/2026_02_02_105034_create_project_types_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->text('detail')->nullable();
+            $table->tinyInteger('is_active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_types');
+    }
+}
