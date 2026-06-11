69ff6945d5e2307c1a5b131a013af6e14f0d34e3
mkview <mastertime7824@gmail.com>
2026-06-11 10:00:24 +0700

    feat: add route and controller method to update user permissions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -332,6 +332,36 @@ class UserController extends Controller
         }
     }
 
+    public function updatePermission(Request $request, $id)
+    {
+        $permissionId = $request->input('permission_id');
+        if ($permissionId === null || trim((string) $permissionId) === '') {
+            return $this->returnErrorData('กรุณาระบุสิทธิ์ผู้ใช้งานให้เรียบร้อย', 404);
+        }
+
+        $permission = Permission::find($permissionId);
+        if (!$permission) {
+            return $this->returnErrorData('ไม่พบสิทธิ์ผู้ใช้งานที่ต้องการแก้ไข', 404);
+        }
+
+        $item = User::find($id);
+        if (!$item) {
+            return $this->returnErrorData('ไม่พบผู้ใช้งานที่ต้องการแก้ไข', 404);
+        }
+
+        DB::beginTransaction();
+        try {
+            $item->permission_id = (int) $permissionId;
+            $item->save();
+
+            DB::commit();
+            return $this->returnSuccess('อัปเดตสิทธิ์ผู้ใช้งานสำเร็จ', $item);
+        } catch (\Throwable $e) {
+            DB::rollback();
+            return $this->returnErrorData('เกิดข้อผิดพลาด กรุณาลองใหม่อีกครั้ง ' . $e->getMessage(), 500);
+        }
+    }
+
     public function getProfileUser(Request $request)
     {
 
--- routes/api.php
+++ routes/api.php
@@ -340,6 +340,7 @@ Route::post('/update_user', [UserController::class, 'update']);
 
 // Update user status (used by settings user list dropdown)
 Route::put('/user/{id}/status', [UserController::class, 'updateStatus']);
+Route::put('/user/{id}/permission', [UserController::class, 'updatePermission']);
 
 Route::resource('user', UserController::class);
 Route::put('/update_password_user/{id}', [UserController::class, 'updatePasswordUser']);
